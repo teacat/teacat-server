9fb2c28fa389cbb3dd3af9a91a04f9462e45970f
Yami Odymel <yamiodymel@gmail.com>
2016-12-27 20:05:52 +0800

    Cleaned up main, everything are now separated

--- main.go
+++ main.go
@@ -11,60 +11,38 @@ import (
 	"github.com/TeaMeow/KitSvc/logging"
 	"github.com/TeaMeow/KitSvc/service"
 
-	"golang.org/x/net/context"
-
 	"github.com/go-kit/kit/log"
-
-	httptransport "github.com/go-kit/kit/transport/http"
-	stdprometheus "github.com/prometheus/client_golang/prometheus"
 )
 
 func main() {
+
+	// Command line flags.
 	var (
 		listen = flag.String("listen", ":8080", "HTTP listen address")
 	)
 	flag.Parse()
 
+	// Load the configurations.
 	conf := config.Load()
 
+	// The logger.
 	var logger log.Logger
 	logger = log.NewLogfmtLogger(os.Stderr)
 	logger = log.NewContext(logger).With("listen", *listen).With("caller", log.DefaultCaller)
 
-	ctx := context.Background()
-
+	// Service and the middlewares.
 	var svc service.Service
-
 	svc = service.Concrete{}
 	svc = logging.CreateMiddleware(logger)(svc)
 	svc = instrumenting.CreateMiddleware(conf)(svc)
 
-	uppercaseHandler := httptransport.NewServer(
-		ctx,
-		makeUppercaseEndpoint(svc),
-		decodeUppercaseRequest,
-		encodeResponse,
-	)
-	lowercaseHandler := httptransport.NewServer(
-		ctx,
-		makeLowercaseEndpoint(svc),
-		decodeLowercaseRequest,
-		encodeResponse,
-	)
-	countHandler := httptransport.NewServer(
-		ctx,
-		makeCountEndpoint(svc),
-		decodeCountRequest,
-		encodeResponse,
-	)
+	// Set the handlers.
+	service.SetHandlers(svc)
 
-	http.Handle("/lowercase", lowercaseHandler)
-	http.Handle("/uppercase", uppercaseHandler)
-	http.Handle("/count", countHandler)
-	http.Handle("/metrics", stdprometheus.Handler())
-
+	// Register the service to the service discovery registry.
 	discovery.Register(&conf, logger)
 
+	// Log and start the HTTP transmission.
 	logger.Log("msg", "HTTP", "addr", *listen)
 	logger.Log("err", http.ListenAndServe(*listen, nil))
 }
