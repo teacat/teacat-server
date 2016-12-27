ee2ccec332cc52e1c53d1a0141e35c4fe29bb231
Yami Odymel <yamiodymel@gmail.com>
2016-12-27 20:06:31 +0800

    Donezo with database connection

--- database/database.go
+++ database/database.go
@@ -1,1 +1,30 @@
 package database
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/TeaMeow/KitSvc/config"
+	"github.com/jinzhu/gorm"
+)
+
+func Connect(c config.Database) *gorm.DB {
+
+	db, err := gorm.Open("mysql", fmt.Sprintf("%s:%s@%s:%d/%s?charset=%s&parseTime=%s&loc=%s",
+		c.User,
+		c.Password,
+		c.Host,
+		c.Port,
+		c.Name,
+		c.Charset,
+		strconv.FormatBool(c.ParseTime),
+		c.Loc,
+	))
+
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	return db
+}
