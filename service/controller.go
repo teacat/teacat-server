7380fe2bfc58e363382258913defe2ae710ea883
Yami Odymel <yamiodymel@gmail.com>
2017-01-01 06:31:32 +0800

    Removed Lowercase from controller

--- service/controller.go
+++ service/controller.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
-	"strings"
 
 	nsq "github.com/bitly/go-nsq"
 )
@@ -18,50 +17,29 @@ var (
 	}
 )
 
-type Payload map[string]interface{}
-
 // StringService 是基於字串的服務。
 type Service interface {
 	Uppercase(string) (string, error)
-	Lowercase(string) (string, error)
 	Count(string) int
 	Test(*nsq.Message)
 }
 
-// stringService 概括了字串服務所可用的函式。
-type Concrete struct {
-	Message *nsq.Producer
-}
-
-// ServiceMiddleware 是處理 StringService 的中介層。
-type Middleware func(Service) Service
-
 // Uppercase 將傳入的字串轉換為大寫。
 func (c Concrete) Uppercase(s string) (string, error) {
 
-	c.Message.Publish("new_user", []byte("test"))
+	//c.Message.Publish("new_user", []byte("test"))
 
-	if s == "" {
-		return "", Err{
-			Message: ErrEmpty,
-		}
+	res, err := c.Model.ToUpper(s)
+	if err != nil {
+		return "", err
 	}
 
-	return strings.ToUpper(s), nil
-}
-
-// Lowercase 將傳入的字串轉換為小寫。
-func (Concrete) Lowercase(s string) (string, error) {
-	if s == "" {
-		return "", ErrEmpty
-	}
-
-	return strings.ToLower(s), nil
+	return res, nil
 }
 
 // Count 計算傳入的字串長度。
-func (Concrete) Count(s string) int {
-	return len(s)
+func (c Concrete) Count(s string) int {
+	return c.Model.Count(s)
 }
 
 func (Concrete) Test(msg *nsq.Message) {
