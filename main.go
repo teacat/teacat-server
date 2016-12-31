310f5aa8464355d2a6ecc1b537f06add9d810685
Yami Odymel <yamiodymel@gmail.com>
2017-01-01 06:30:23 +0800

    Now load the database

--- main.go
+++ main.go
@@ -6,17 +6,16 @@ import (
 	"os"
 
 	"github.com/TeaMeow/KitSvc/config"
+	"github.com/TeaMeow/KitSvc/database"
 	"github.com/TeaMeow/KitSvc/discovery"
 	"github.com/TeaMeow/KitSvc/instrumenting"
 	"github.com/TeaMeow/KitSvc/logging"
 	"github.com/TeaMeow/KitSvc/messaging"
 	"github.com/TeaMeow/KitSvc/service"
-
 	"github.com/go-kit/kit/log"
 )
 
 func main() {
-
 	// Command line flags.
 	var (
 		listen = flag.String("listen", ":8080", "HTTP listen address")
@@ -24,26 +23,32 @@ func main() {
 	flag.Parse()
 
 	// Load the configurations.
-	conf := config.Load()
+	conf := config.Load("./")
 
 	// The logger.
 	var logger log.Logger
 	logger = log.NewLogfmtLogger(os.Stderr)
 	logger = log.NewContext(logger).With("listen", *listen).With("caller", log.DefaultCaller)
 
+	// Connect to the database.
+	db := database.Connect(conf.Database)
+
+	// Create the model with the database connection.
+	model := service.Model{DB: db}
+
 	// Register to the message system.
 	msg := messaging.Create(conf, logger)
 
 	// Service and the middlewares.
 	var svc service.Service
-	svc = service.Concrete{Message: msg.Producer}
+	svc = service.Concrete{Message: msg.Producer, Model: model}
 	svc = logging.CreateMiddleware(logger)(svc)
 	svc = instrumenting.CreateMiddleware(conf)(svc)
 
-	//
+	// Set the message handlers.
 	msg.SetHandlers(svc)
 
-	// Set the handlers.
+	// Set the service handlers.
 	service.SetHandlers(svc)
 
 	// Register the service to the service discovery registry.
