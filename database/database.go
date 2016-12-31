a6755adff377f39b81243dfc2873b518ef9b1448
Yami Odymel <yamiodymel@gmail.com>
2017-01-01 06:31:14 +0800

    Added the mysql driver

--- database/database.go
+++ database/database.go
@@ -5,12 +5,13 @@ import (
 	"strconv"
 
 	"github.com/TeaMeow/KitSvc/config"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 )
 
 func Connect(c config.Database) *gorm.DB {
 
-	db, err := gorm.Open("mysql", fmt.Sprintf("%s:%s@%s:%d/%s?charset=%s&parseTime=%s&loc=%s",
+	db, err := gorm.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=%s&parseTime=%s&loc=%s",
 		c.User,
 		c.Password,
 		c.Host,
