c437716ace1e8644c26a1d0a54793859853881e4
Yami Odymel <yamiodymel@gmail.com>
2017-01-09 06:32:00 +0800

    Service with event publish, edited catchEvent func

--- service/service.go
+++ service/service.go
@@ -5,15 +5,23 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
+
+	"github.com/jetbasrawi/go.geteventstore"
 )
 
 // Error codes returned by failures
 var (
-	// ErrEmpty will returned if the string is empty.
+	// ErrEmpty is returned if the string is empty.
 	ErrEmpty = ErrInfo{
 		Text:   errors.New("The string is empty."),
 		Status: http.StatusBadRequest,
-		Code:   "str_empty",
+		Code:   "string_empty",
+	}
+	// ErrEvent is returned if the error occurred while we're publishing the event.
+	ErrEvent = ErrInfo{
+		Text:   errors.New("Error occurred while publishing the event."),
+		Status: http.StatusInternalServerError,
+		Code:   "event_error",
 	}
 )
 
@@ -21,7 +29,7 @@ var (
 type Service interface {
 	Uppercase(string) (string, error)
 	Count(string) int
-	CatchEvent(map[string]interface{}, map[string]string)
+	CatchUppercase(map[string]interface{}, map[string]interface{})
 }
 
 // Count counts the length of the string.
@@ -35,15 +43,31 @@ func (svc service) Uppercase(s string) (string, error) {
 		return "", Err{Message: ErrEmpty}
 	}
 
+	// Converts the string to uppercase.
 	u := strings.ToUpper(s)
 
+	// Create the string record in the database.
 	svc.Store.CreateString(s, u)
 
+	// Publish the uppercase event to the event store.
+	if err := svc.ES.NewStreamWriter("uppercase").Append(nil, goes.NewEvent(
+		"",
+		"",
+		H{"input": s, "output": u}.MapString(),
+		H{}.MapString(),
+	)); err != nil {
+		return "", Err{Message: ErrEvent}
+	}
+
+	// Get the last record from the database.
+	fmt.Println("Last record:")
 	fmt.Println(svc.Store.GetLastString())
 
 	return u, nil
 }
 
-func (svc service) CatchEvent(body map[string]interface{}, meta map[string]string) {
-	fmt.Println(body["test"])
+// CatchUppercase catches the uppercase event, and print the event data.
+func (svc service) CatchUppercase(body map[string]interface{}, meta map[string]interface{}) {
+	fmt.Println("Event received:")
+	fmt.Println("Input:" + body["input"].(string) + " | Output:" + body["output"].(string))
 }
