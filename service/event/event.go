5c2d7436c80f0bc38823a546f548f85a137e6e76
Yami Odymel <yamiodymel@gmail.com>
2017-01-15 22:45:06 +0800

    New structure

--- service/event/event.go
+++
@@ -1,9 +0,0 @@
-package event
-
-import "github.com/jetbasrawi/go.geteventstore"
-
-type Option struct {
-	Client *goes.Client
-	Stream string
-	Meta   map[string]string
-}
--- service/model/string.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type String struct {
-	ID     int
-	Input  string `gorm:"not null"`
-	Output string `gorm:"not null"`
-}
