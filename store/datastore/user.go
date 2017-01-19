8984737a000446e4529063bbae1302d987317317
Yami Odymel <yamiodymel@gmail.com>
2017-01-19 22:12:36 +0800

    Fixed user store

--- store/datastore/user.go
+++ store/datastore/user.go
@@ -2,20 +2,24 @@ package datastore
 
 import "github.com/TeaMeow/KitSvc/model"
 
-func (db *datastore) CreateUser(user *model.User) error {
-	return db.Create(user).Error
+//
+func (db *datastore) CreateUser(u *model.User) error {
+	return db.Create(&u).Error
 }
 
-func (db *datastore) GetUser(id int) (*model.User, error) {
-	var user *model.User
-	d := db.Where(&model.User{ID: id}).First(&user)
-	return user, d.Error
+//
+func (db *datastore) GetUser(username string) (*model.User, error) {
+	u := &model.User{}
+	d := db.Where(&model.User{Username: username}).First(&u)
+	return u, d.Error
 }
 
+//
 func (db *datastore) DeleteUser(id int) error {
 	return db.Delete(&model.User{ID: id}).Error
 }
 
-func (db *datastore) UpdateUser(user *model.User) error {
-	return db.Save(&user).Error
+//
+func (db *datastore) UpdateUser(u *model.User) error {
+	return db.Save(&u).Error
 }
