e62aa8c3fabf5db58540da8d0f220f74bcad469c
Yami Odymel <yamiodymel@gmail.com>
2017-01-19 22:14:02 +0800

    Expose the store key name

--- store/context.go
+++ store/context.go
@@ -4,7 +4,7 @@ import (
 	"golang.org/x/net/context"
 )
 
-const key = "store"
+const Key = "store"
 
 // Setter defines a context that enables setting values.
 type Setter interface {
@@ -13,11 +13,11 @@ type Setter interface {
 
 // FromContext returns the Store associated with this context.
 func FromContext(c context.Context) Store {
-	return c.Value(key).(Store)
+	return c.Value(Key).(Store)
 }
 
 // ToContext adds the Store to this context if it supports
 // the Setter interface.
 func ToContext(c Setter, store Store) {
-	c.Set(key, store)
+	c.Set(Key, store)
 }
