79b96ee8e0c0d5e02ad90159b09c9da93970627e
Yami Odymel <yamiodymel@gmail.com>
2017-02-10 12:49:40 +0800

    Added client test functions

--- client/client_impl.go
+++ client/client_impl.go
@@ -6,6 +6,7 @@ import (
 )
 
 const (
+	pathAuth          = "%s/auth"
 	pathUser          = "%s/user"
 	pathSpecifiedUser = "%s/user/%s"
 )
@@ -28,18 +29,37 @@ func (c *client) PostUser(in *model.User) (out *model.User, err []error) {
 	return
 }
 
-func (c *client) GetUser() {
-
+func (c *client) GetUser(username string) (out *model.User, err []error) {
+	_, _, err = gorequest.
+		New().
+		Get(uri(pathSpecifiedUser, c.base, username)).
+		EndStruct(&out)
+	return
 }
 
-func (c *client) PutUser() {
-
+func (c *client) PutUser(id int, in *model.User) (out *model.User, err []error) {
+	_, _, err = gorequest.
+		New().
+		Put(uri(pathSpecifiedUser, c.base, id)).
+		Send(in).
+		EndStruct(&out)
+	return
 }
 
-func (c *client) DeleteUser() {
-
+func (c *client) DeleteUser(id int, in *model.User) (out *model.User, err []error) {
+	_, _, err = gorequest.
+		New().
+		Delete(uri(pathSpecifiedUser, c.base, id)).
+		Send(in).
+		EndStruct(&out)
+	return
 }
 
-func (c *client) PostAuth() {
-
+func (c *client) PostAuth(in *model.User) (body string, err []error) {
+	_, body, err = gorequest.
+		New().
+		Post(uri(pathAuth, c.base)).
+		Send(in).
+		End()
+	return
 }
