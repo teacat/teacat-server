768f5fddcb5017f5e761fe4db5873f960bad089f
Yami Odymel <yamiodymel@gmail.com>
2017-02-11 22:54:52 +0800

    Added compare, encrypt in user model

--- model/user.go
+++ model/user.go
@@ -1,11 +1,28 @@
 package model
 
-import validator "gopkg.in/go-playground/validator.v9"
+import (
+	"github.com/TeaMeow/KitSvc/shared/auth"
+	validator "gopkg.in/go-playground/validator.v9"
+)
 
 type User struct {
 	ID       int    `json:"id"`
 	Username string `json:"username" gorm:"not null" binding:"required" validate:"min=1,max=32"`
 	Password string `json:"password" gorm:"not null" binding:"required" validate:"min=8,max=128"`
+}
+
+type Token struct {
+	Token string `json:"token"`
+}
+
+func (u *User) Compare(pwd string) (err error) {
+	err = auth.Compare(u.Password, pwd)
+	return
+}
+
+func (u *User) Encrypt() (err error) {
+	u.Password, err = auth.Encrypt(u.Password)
+	return
 }
 
 func (u *User) Validate() error {
