bfa40ca41c13b36325863888d152bb31bedddf8a
Yami Odymel <yamiodymel@gmail.com>
2017-02-11 22:54:29 +0800

    Added token client, error handler

--- client/client_impl.go
+++ client/client_impl.go
@@ -23,55 +23,74 @@ func NewClient(uri string) Client {
 	return &client{base: uri}
 }
 
+func NewClientToken(uri, token string) Client {
+	return &client{token: token, base: uri}
+}
+
+func handler(resp gorequest.Response, b string, e []error, out interface{}) (errs []error) {
+	errs = append(errs, e...)
+	if resp != nil && resp.StatusCode < 300 {
+		if err := json.Unmarshal([]byte(b), &out); err != nil {
+			errs = append(errs, err)
+		}
+		return
+	}
+	errs = append(errs, errors.New(b))
+
+	return
+}
+
 func (c *client) PostUser(in *model.User) (out *model.User, errs []error) {
-	gorequest.
+	resp, b, e := gorequest.
 		New().
 		Post(uri(pathUser, c.base)).
 		Send(in).
-		End(func(resp gorequest.Response, b string, e []error) {
-			if resp.StatusCode < 300 {
-				if err := json.Unmarshal([]byte(b), &out); err != nil {
-					panic(err)
-				}
-				return
-			}
-			errs = e
-			errs = append(errs, errors.New(b))
-		})
+		End()
+
+	errs = handler(resp, b, e, &out)
 	return
 }
 
-func (c *client) GetUser(username string) (out *model.User, err []error) {
-	_, _, err = gorequest.
+func (c *client) GetUser(username string) (out *model.User, errs []error) {
+	resp, b, e := gorequest.
 		New().
 		Get(uri(pathSpecifiedUser, c.base, username)).
-		EndStruct(&out)
+		End()
+
+	errs = handler(resp, b, e, &out)
 	return
 }
 
-func (c *client) PutUser(id int, in *model.User) (out *model.User, err []error) {
-	_, _, err = gorequest.
+func (c *client) PutUser(id int, in *model.User) (out *model.User, errs []error) {
+	resp, b, e := gorequest.
 		New().
 		Put(uri(pathSpecifiedUser, c.base, id)).
+		Set("Authorization", "Bearer "+c.token).
 		Send(in).
-		EndStruct(&out)
+		End()
+
+	errs = handler(resp, b, e, &out)
 	return
 }
 
-func (c *client) DeleteUser(id int, in *model.User) (out *model.User, err []error) {
-	_, _, err = gorequest.
+func (c *client) DeleteUser(id int, in *model.User) (out *model.User, errs []error) {
+	resp, b, e := gorequest.
 		New().
+		Set("Authorization", "Bearer "+c.token).
 		Delete(uri(pathSpecifiedUser, c.base, id)).
 		Send(in).
-		EndStruct(&out)
+		End()
+
+	errs = handler(resp, b, e, &out)
 	return
 }
 
-func (c *client) PostAuth(in *model.User) (body string, err []error) {
-	_, body, err = gorequest.
+func (c *client) PostAuth(in *model.User) (out *model.Token, errs []error) {
+	resp, b, e := gorequest.
 		New().
 		Post(uri(pathAuth, c.base)).
 		Send(in).
 		End()
+	errs = handler(resp, b, e, &out)
 	return
 }
