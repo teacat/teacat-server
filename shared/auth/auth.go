5e07bddf262044807db2319a51f76ff63f7e93a9
Yami Odymel <yamiodymel@gmail.com>
2017-02-11 22:55:29 +0800

    auth.Encrypt now takes plain string

--- shared/auth/auth.go
+++ shared/auth/auth.go
@@ -2,11 +2,9 @@ package auth
 
 import "golang.org/x/crypto/bcrypt"
 
-func Encrypt(source *string) error {
-	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(*source), bcrypt.DefaultCost)
-	hashedString := string(hashedBytes)
-	source = &hashedString
-	return err
+func Encrypt(source string) (string, error) {
+	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(source), bcrypt.DefaultCost)
+	return string(hashedBytes), err
 }
 
 func Compare(hashedPassword, password string) error {
