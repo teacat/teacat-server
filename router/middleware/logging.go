b8610b81bf2153cf93521e3a9052b38b9182a347
Yami Odymel <yamiodymel@gmail.com>
2017-02-16 01:13:24 +0800

    New format for route logging

--- router/middleware/logging.go
+++ router/middleware/logging.go
@@ -1,42 +1,47 @@
 package middleware
 
 import (
-	"os"
+	"fmt"
 	"time"
 
 	"github.com/Sirupsen/logrus"
+	"github.com/TeaMeow/KitSvc/module/logger"
 	"github.com/gin-gonic/gin"
+	"github.com/willf/pad"
 )
 
 func Logging() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		logger := logrus.StandardLogger()
+
 		start := time.Now().UTC()
 		path := c.Request.URL.Path
 		c.Next()
+
+		if path == "/metrics" || path == "/sd/health" || path == "/sd/ram" || path == "/sd/cpu" || path == "/sd/disk" {
+			return
+		}
+
 		end := time.Now().UTC()
 		latency := end.Sub(start)
 
-		formatter := &logrus.TextFormatter{
-			FullTimestamp:   true,
-			TimestampFormat: "2006-01-02-15:04:05",
-		}
-		logrus.SetFormatter(formatter)
-		//logrus.SetFormatter(&logrus.JSONFormatter{})
-		file, err := os.OpenFile("./logrus.log", os.O_APPEND|os.O_WRONLY, 0666)
-		if err == nil {
-			logger.Out = file
-		} else {
-			logger.Info("Failed to log to file, using default stderr")
-		}
+		status := c.Writer.Status()
+		method := c.Request.Method
+		ip := c.ClientIP()
+		userAgent := c.Request.UserAgent()
 
-		logger.WithFields(logrus.Fields{
-			"status":     c.Writer.Status(),
-			"method":     c.Request.Method,
-			"path":       path,
-			"ip":         c.ClientIP(),
-			"duration":   latency,
-			"user_agent": c.Request.UserAgent(),
-		}).Info()
+		logger.InfoFields(fmt.Sprintf("%d | %s | %s | %s %s",
+			status,
+			pad.Right(latency.String(), 13, " "),
+			pad.Right(ip, 12, " "),
+			pad.Right(method, 5, " "),
+			pad.Right(path, 15, " "),
+		), logrus.Fields{
+			//"status":     status,
+			//"method":     method,
+			//"path":       path,
+			//"ip":         ip,
+			//"duration":   latency,
+			"user_agent": userAgent,
+		})
 	}
 }
