14bca738dfe74c76c37ffbd2e7f73363e0aa4334
Yami Odymel <yamiodymel@gmail.com>
2017-02-17 04:08:25 +0800

    Cleaned up

--- errno/errno.go
+++ errno/errno.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Err represents an error, `Path`, `Line`, `Code` will be automatically filled.
 type Err struct {
 	Code       string
 	Message    string
@@ -16,14 +17,17 @@ type Err struct {
 	Line       int
 }
 
+// Error returns the error message.
 func (e *Err) Error() string {
 	return e.Message
 }
 
+// Error returns the error struct by the error code.
 func Error(code string) *Err {
 	if val, ok := errs[code]; ok {
 		_, fn, line, _ := runtime.Caller(1)
 
+		// Fill the error occurred path, line, code.
 		val.Code = code
 		val.Path = strings.Replace(fn, os.Getenv("GOPATH"), "", -1)
 		val.Line = line
@@ -33,6 +37,7 @@ func Error(code string) *Err {
 	return nil
 }
 
+// Abort the current request with the specified error code.
 func Abort(code string, err error, c *gin.Context) {
 	c.Error(err)
 	c.Error(Error(code))
